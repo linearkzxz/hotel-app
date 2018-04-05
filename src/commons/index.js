c933745976187afb2fcc311b3f6f789996be2317
K <K@Nopppadols-MacBook-Air.local>
2018-04-06 02:49:54 +0700

    remove unuse file

--- src/commons/index.js
+++ src/commons/index.js
@@ -1,9 +1,7 @@
-import FieldGroup from './FieldGroup'
 import RoomCard from './RoomCard'
 import HotelCard from './HotelCard'
 
 export {
-  FieldGroup,
   RoomCard,
   HotelCard,
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import hotelReducer from './hotelReducer'
-import cartReducer from './cartReducer'
 
 export default combineReducers({
   hotel: hotelReducer,
-  cart: cartReducer,
 })
